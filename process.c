96463265c266401c799f9ea3c951989996b1b7d0
enzo <enzo776b@outlook.fr>
2025-12-07 21:52:47 +0100

    maj process

--- process.c
+++ process.c
@@ -9,9 +9,42 @@ typedef struct {
 } process_t;
 
 
+
 typedef struct {
     process_t *list;        // Tableau
     int count;              // Nombre actuel d'éléments
     int capacity;           // Taille allouée en mémoire
 } process_list_t;
+
+
+
+LISTE DES INFORMATIONS A AFFICHER POUR CHAQUE PROCESSUS :
+
+PID - OK
+USER - OK
+PRI - ?
+NI - ?
+VIRT - ?
+RES - ?
+SHR - ?
+S - OK
+CPU% - OK
+MEM% - OK
+TIME+ - ?
+COMMAND - OK
+
+il manque :
+- PRI (priorité)
+- NI (nice value)
+- VIRT (taille mémoire virtuelle)
+- RES (taille mémoire résidente)
+- SHR (taille mémoire partagée)
+- TIME+ (temps CPU total utilisé par le processus)
+
 */
+
+
+
+int main() {
+    
+}
