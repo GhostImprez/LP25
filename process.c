55d5e8c118593a1ca9cb870a373c80bf16652043
enzo <enzo776b@outlook.fr>
2025-12-07 20:58:45 +0100

    creation du fichier process

---
+++ process.c
@@ -0,0 +1,17 @@
+/*
+typedef struct {
+    pid_t pid;
+    char user[32];
+    char command[256];
+    process_state_e state;
+    double cpu_usage;       // %
+    double mem_usage;       // %
+} process_t;
+
+
+typedef struct {
+    process_t *list;        // Tableau
+    int count;              // Nombre actuel d'éléments
+    int capacity;           // Taille allouée en mémoire
+} process_list_t;
+*/
