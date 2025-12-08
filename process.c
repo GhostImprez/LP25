cc772001a9ee67cd02393a1dcc72a1a19fc6fae1
enzo <enzo776b@outlook.fr>
2025-12-08 22:29:21 +0100

    commit_maj code

--- process.c
+++ process.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <ctype.h>
+
 /*
 typedef struct {
     pid_t pid;
@@ -41,10 +47,49 @@ il manque :
 - SHR (taille mémoire partagée)
 - TIME+ (temps CPU total utilisé par le processus)
 
+Informations retenues :
+- PID
+- USER
+- S (state)
+- CPU%
+- MEM%
+- TIME+ -----------
+- COMMAND
+
 */
+
+
+typedef enum {
+    STATE_RUNNING = 'R',
+    STATE_SLEEPING = 'S',
+    STATE_ZOMBIE = 'Z',
+    STATE_STOPPED = 'T',
+    STATE_UNKNOWN = '?'
+} process_state_e;
+
+
+typedef struct {
+    pid_t pid;
+    char user[32];
+    char command[256];
+    process_state_e state;
+    double cpu_usage;       // %
+    double mem_usage;       // %
+} process_t;
+
+
+
+typedef struct {
+    process_t *list;        // Tableau
+    int count;              // Nombre actuel d'éléments
+    int capacity;           // Taille allouée en mémoire
+} process_list_t;
+
 
 
 
 int main() {
+
+    return
     
 }
